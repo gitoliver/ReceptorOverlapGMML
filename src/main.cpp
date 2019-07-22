b142347007132400ef30c8c69ccc5619844f2332
oliver <olivercgrant@gmail.com>
2019-07-22 13:15:27 +0200

    Working version with Strep to HA angle included in the calculation. Still have a bug with the mirrored Strep sites giving opposite angles

--- src/main.cpp
+++ src/main.cpp
@@ -17,6 +17,8 @@
 #include "../includes/bead_residues.h"
 
 //typedef std::vector<GeometryTopology::Coordinate> CoordinateVector;
+double GetAngleBetweenVectors(Eigen::Vector3d v1, Eigen::Vector3d v2);
+Atom* FindCAAtomOfResidueWithNumber(ResidueVector residues, std::string query_residue_number);
 AssemblyVector loadPDBFilesFromDirectory(std::string directory);
 AssemblyVector loadPDBFilesFromDirectory(std::string directory, MolecularModeling::Assembly combined_assembly);
 MolecularModeling::Residue* seek_alycone_residue_connected_to_residue(MolecularModeling::Residue *residue, MolecularModeling::Assembly *assembly);
@@ -26,6 +28,18 @@ MolecularModeling::Residue* recursively_seek_alycone_residue(MolecularModeling::
 int main(int argc, char *argv[])
 {
 
+    /* Ok this is frankencode now. Originally adapted from Superimposing ERManI code, I wanted to make it general, but failed. The ERManI code
+     * involved superimposing ERManI to a glycan and checking overlap. I needed to report the protein residue the glycan was attached to. So I had to
+     * first find it and then convert it to HXB2 (HIV env) numbering. All that stuff I don't need but was attempting to remain flexible.
+     * For this code, I have simulations (multiple snapshots/coordinate sets) of Strepavidin, which has four binding sites and the same biotinylated glycan
+     * in each site. I want to do three things. First superimpose influenza HA to each glycan in each site and check if there overlaps between strep
+     * and the HA (preventing binding). Second I want to measure the distance between the C1 of the Gal residue of the biotinylated glycan to the middle
+     * of the strep molecule, as my collaborater is interested in the spacing required for high affinity binding (he knows this) and how high affinity
+     * binding is being acheived (we can deduce from the spacing). Third, but related to the other two, I want to know the angle between the HA and the strep
+     * as the HA is embedded in a viral coat in the actual experiment, and so if I predict that it can bind or am measureing distances from glycans, but
+     * that shape wouldn't allow an embedded HA to bind then it shouldn't be counted. The first two questions could be solved with separate code. The third
+     * question is really a pre-screen that I could do while doing the overlap calculation. Anyway I do them all in here for whatever reason that is
+     * unclear to me now.
 
 
 /* What do we want to know?
@@ -59,11 +73,15 @@ int main(int argc, char *argv[])
     // Hardcoding as no time and this is too particular to be generalisable anyway. These first two points define the line that runs through the center
     // of the Strepavidin molecule.
 
-    GeometryTopology::Coordinate receptorDisectingLinepoint1(40.1895, 21.084, 7.041);
-    GeometryTopology::Coordinate receptorDisectingLinepoint2(6.096, 21.8635, -6.911);
-    GeometryTopology::Coordinate jola(1.0, -15.0, 1.0);
+    GeometryTopology::Coordinate streptavidinDisectingLinepoint2(40.1895, 21.084, 7.041);
+    GeometryTopology::Coordinate streptavidinDisectingLinepoint1(6.096, 21.8635, -6.911);
+    GeometryTopology::Coordinate jola(1.0, -15.0, 1.0); // This is just me checking that this next function works
 
-    std::cout << "Distance from jola to line is " << GeometryTopology::calculateDistanceFromPointToLineBetweenTwoPoints(jola, receptorDisectingLinepoint1, receptorDisectingLinepoint2) << "\n";
+    // Now for the angle between strep and HA, need a vector for the step line and can calculate now as it is fixed.
+    Eigen::Vector3d strepVector3D(streptavidinDisectingLinepoint1.GetX() - streptavidinDisectingLinepoint2.GetX(), streptavidinDisectingLinepoint1.GetY() - streptavidinDisectingLinepoint2.GetY(),streptavidinDisectingLinepoint1.GetZ() - streptavidinDisectingLinepoint2.GetZ() );
+    Eigen::Vector3d strepVector3DReversed(streptavidinDisectingLinepoint2.GetX() - streptavidinDisectingLinepoint1.GetX(), streptavidinDisectingLinepoint2.GetY() - streptavidinDisectingLinepoint1.GetY(),streptavidinDisectingLinepoint2.GetZ() - streptavidinDisectingLinepoint1.GetZ() );
+
+  //  std::cout << "Distance from jola to line is " << GeometryTopology::calculateDistanceFromPointToLineBetweenTwoPoints(jola, streptavidinDisectingLinepoint1, streptavidinDisectingLinepoint2) << "\n";
 
     typedef std::vector<GeometryTopology::Coordinate*> CoordinateVector;
 
@@ -74,9 +92,9 @@ int main(int argc, char *argv[])
     // Reading input file                             //
     //************************************************//
 
-    StringVector target_residue_list;
+    StringVector target_residue_list, receptor_vector_residue_list;
     std::string buffer, snapshot_directory, template_pdb, ligand_pdb, receptor_pdb, residue_conversion_map;
-    bool isResidueConversionMap = false;
+    bool isResidueConversionMap = false, isReceptorVectorResidueList = false;
     std::ifstream infile (workingDirectory + "/" + inputsDirectory + "/" + inputFile);
     if (!infile)
     {
@@ -95,6 +113,17 @@ int main(int argc, char *argv[])
             getline(infile, receptor_pdb);
         if(strInput == "Template PDB:")
             getline(infile, template_pdb);
+        if(strInput == "Receptor Vector Residue Numbers:")
+        {
+            isReceptorVectorResidueList = true;
+            getline(infile, buffer);
+            while(buffer != "END")
+            {
+                receptor_vector_residue_list.push_back(buffer);
+                getline(infile, buffer);
+            }
+            buffer.clear();
+        }
         if(strInput == "Residue Conversion Map:")
         {
             isResidueConversionMap = true;
@@ -108,6 +137,7 @@ int main(int argc, char *argv[])
                 target_residue_list.push_back(buffer);
                 getline(infile, buffer);
             }
+            buffer.clear();
         }
     }
 
@@ -162,6 +192,15 @@ int main(int argc, char *argv[])
     std::cout << "......" << std::endl;
 
 
+    Atom *receptorVector3DAtom1, *receptorVector3DAtom2;
+    if(isReceptorVectorResidueList)
+    {
+        // Get atom pointers to CA atoms in the receptor (HA protein) that will be used for the HA oreintation vector
+        receptorVector3DAtom1 = FindCAAtomOfResidueWithNumber(receptor.GetResidues(), receptor_vector_residue_list.at(0));
+        receptorVector3DAtom2 = FindCAAtomOfResidueWithNumber(receptor.GetResidues(), receptor_vector_residue_list.at(1));
+    }
+
+
     // For each target residue, go through each coordinate set (each snapshot) and get the coordinates of each atom.
     // Then, superimpose the ligand atoms to each snapshot with receptor and calculate overlap.
     double overlap = 0.0;
@@ -205,23 +244,23 @@ int main(int argc, char *argv[])
             //std::cout << ".";
             //std::cout << receptors_ligand_coordinates.size() << "vs" << target_atoms_coordinates.size() << std::endl;
             gmml::Superimpose(receptors_ligand_coordinates, target_atoms_coordinates, receptorCoordinates);
-            std::cout << GeometryTopology::calculateDistanceFromPointToLineBetweenTwoPoints(target_residue_C1_atom->GetCoordinates().at(i), receptorDisectingLinepoint1, receptorDisectingLinepoint2) << ", " << std::flush;
+            std::cout << GeometryTopology::calculateDistanceFromPointToLineBetweenTwoPoints(target_residue_C1_atom->GetCoordinates().at(i), streptavidinDisectingLinepoint1, streptavidinDisectingLinepoint2) << ", " << std::flush;
             //std::cout << "..";
 
             // WRITE OUT PDB FILES EVERY STEP
-//            std::stringstream ss, ss1;
-//           // ss << workingDirectory << "/receptor_" << i << "_" << aglycone->GetNumber() << ".pdb";
-//            ss << workingDirectory << "/receptor_" << i << "_" << converted_residue_number << ".pdb";
-//            PdbFileSpace::PdbFile *outputPdbFile = receptor.BuildPdbFileStructureFromAssembly(-1,0);
-//            outputPdbFile->Write(ss.str());
-//            ss1 << workingDirectory << "/ligand_" << i << "_" << converted_residue_number << ".pdb";
-//            PdbFileSpace::PdbFile *outputPdbFile1 = receptors_ligand.BuildPdbFileStructureFromAssembly(-1, 0);
-//            outputPdbFile1->Write(ss1.str());
+            std::stringstream ss, ss1;
+           // ss << workingDirectory << "/receptor_" << i << "_" << aglycone->GetNumber() << ".pdb";
+            ss << workingDirectory << "/receptor_" << i << "_" << converted_residue_number << ".pdb";
+            PdbFileSpace::PdbFile *outputPdbFile = receptor.BuildPdbFileStructureFromAssembly(-1,0);
+            outputPdbFile->Write(ss.str());
+            ss1 << workingDirectory << "/ligand_" << i << "_" << converted_residue_number << ".pdb";
+            PdbFileSpace::PdbFile *outputPdbFile1 = receptors_ligand.BuildPdbFileStructureFromAssembly(-1, 0);
+            outputPdbFile1->Write(ss1.str());
             // END OF WRITE OUT PDB FILES EVERY STEP
 
             //std::cout << gmml::CalculateAtomicOverlaps(receptor.GetAllAtomsOfAssembly(), assembly_of_pdbs.GetAllAtomsOfAssembly()) << ", " << std::flush;
             overlap = beads::Calculate_bead_overlaps(receptorBeads, proteinBeads);
-            std::cout << overlap << ", " << "\n";
+            std::cout << overlap << ", ";
 //            if (overlap <= 0.5)
 //            {
 //                // Get shapes with good HA orientation
@@ -230,6 +269,22 @@ int main(int argc, char *argv[])
 //                // Calculate minimum distance from point to line
 //                //GeometryTopology::calculateDistanceFromPointToLineBetweenTwoPoints(queryPoint, linePointA, linePointB);
 //            }
+            if(isReceptorVectorResidueList)
+            {
+                Eigen::Vector3d receptorVector3D(receptorVector3DAtom2->GetCoordinate()->GetX() - receptorVector3DAtom1->GetCoordinate()->GetX(),
+                                                 receptorVector3DAtom2->GetCoordinate()->GetY() - receptorVector3DAtom1->GetCoordinate()->GetY(),
+                                                 receptorVector3DAtom2->GetCoordinate()->GetZ() - receptorVector3DAtom1->GetCoordinate()->GetZ());
+               // if()
+                std::cout << GetAngleBetweenVectors(strepVector3D, receptorVector3D) << "<- Angle, " << i << "<- i\n";
+
+              //  std::cout << GetAngleBetweenVectors(strepVector3DReversed, receptorVector3D) << "<- Angle, " << i << "<- i\n";
+
+                // Print out ATOM cards for testing in VMD
+                std::cout << std::fixed << std::setprecision(3);
+                std::cout << "ATOM      1 CA1  HA1     1    " << std::setw(8) << receptorVector3DAtom1->GetCoordinate()->GetX() << std::setw(8) << receptorVector3DAtom1->GetCoordinate()->GetY() << std::setw(8) << receptorVector3DAtom1->GetCoordinate()->GetZ() << "\n";
+                std::cout << "ATOM      2 CA2  HA2     2    " << std::setw(8) << receptorVector3DAtom2->GetCoordinate()->GetX() << std::setw(8) << receptorVector3DAtom2->GetCoordinate()->GetY() << std::setw(8) << receptorVector3DAtom2->GetCoordinate()->GetZ() << "\n";
+                std::cout << "ATOM      3 ST1  ST1     3    " << std::setw(8) << streptavidinDisectingLinepoint1.GetX() << std::setw(8) << streptavidinDisectingLinepoint1.GetY() << std::setw(8) << streptavidinDisectingLinepoint1.GetZ() << "\n";
+                std::cout << "ATOM      4 ST2  ST2     4    " << std::setw(8) << streptavidinDisectingLinepoint2.GetX() << std::setw(8) << streptavidinDisectingLinepoint2.GetY() << std::setw(8) << streptavidinDisectingLinepoint2.GetZ() << "\n";            }
         }
         std::cout << "\n";
     }
@@ -252,6 +307,52 @@ int main(int argc, char *argv[])
 //        return average_coordinate;
 //    }
 //}
+
+//// Ignoring chain IDs. Rewrite or check other code for that.
+//ResidueVector FindResiduesWithResidueNumber(ResidueVector residues, std::string query_residue_number)
+//{
+//    ResidueVector found_residues;
+//    for(auto &residue : residues)
+//    {
+//        if (residue->GetNumber().compare(query_residue_number)==0)
+//        {
+//            found_residues.push_back(residue);
+//        }
+//    }
+//    return found_residues;
+//}
+
+//Eigen::Vector3d calculateVector3DFromResidueSets(ResidueVector residueSet1, ResidueVector residueSet2)
+//{
+//    AtomVector atomSet1, atomSet2;
+//    for(auto &residue : residueSet1)
+//        atomSet1.push_back(residue->GetAtom("CA")); // Stop judging me I just want this to end.
+//    for(auto &residue : residueSet2)
+//        atomSet2.push_back(residue->GetAtom("CA"));
+
+//    GeometryTopology::CoordinateVector
+//}
+
+double GetAngleBetweenVectors(Eigen::Vector3d v1, Eigen::Vector3d v2)
+{
+    // angle = arccos ( v1.v2 / ||v1||||V2||) // "." is dot product. "||" is magnitude. norm() function gives magnitude
+    return ( (acos( v1.dot(v2) / (v1.norm() * v2.norm()))) * (180 / gmml::PI_RADIAN)); // calculate angle, then convert to degrees
+}
+
+Atom* FindCAAtomOfResidueWithNumber(ResidueVector residues, std::string query_residue_number)
+{
+    Atom* returning_atom_pointer = NULL;
+    for(auto &residue : residues)
+    {
+        if (residue->GetNumber().compare(query_residue_number)==0)
+        {
+            returning_atom_pointer = residue->GetAtom("CA");
+        }
+    }
+    return returning_atom_pointer;
+}
+
+
 
 AssemblyVector loadPDBFilesFromDirectory(std::string directory)
 {
